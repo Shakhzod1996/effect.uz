63443b0dc71ba06cc5bbdf82dcbf4f9b7f8f61e4
Shakhzod1996 <shaxzod1996shax@gmail.com>
2022-08-06 09:29:59 +0500

    qazxcv

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const token = "LAEQ2GUBlv8tmxCdFRvq0NhiQrGgBJlEGifjvBz4e0ZHeJZ6Dg";
 
-// ?Currency RequestLAEQ2GUBlv8tmxCdFRvq0NhiQrGgBJlEGifjvBz4e0ZHeJZ6Dg
+// ?Currency Request
 const Currency_Api_USD =
   "https://api.fastforex.io/fetch-all?api_key=8f88f1b08d-a22ea0a6b7-rfs5m2";
 const Currency_Api_EUR =
--- src/components/MainPage/NewsItem/NewsItems.jsx
+++ src/components/MainPage/NewsItem/NewsItems.jsx
@@ -3,8 +3,7 @@ import cart from "../../../assets/images/cart.png";
 import pab from "../../../assets/images/pub.jfif";
 import mess from "../../../assets/images/mess.png";
 
-export default function NewsItem({item}) {
-
+export default function NewsItem({ item }) {
   return (
     <div className="technology-item">
       <p>{item.category_id}</p>
@@ -13,12 +12,13 @@ export default function NewsItem({item}) {
         <h4>{item.title_uz.substring(0, 45)}...</h4>
         <div className="izoh-content">
           <p>
-            <img src={cart} alt="img" />{item.created_date}
+            <img src={cart} alt="img" />
+            {item.created_date}
           </p>{" "}
-            <p className="bot-absolute">
-            | <img className="mess-img" src={mess} alt="img" /> {item.comment_count} izoh
+          <p className="bot-absolute">
+            | <img className="mess-img" src={mess} alt="img" />{" "}
+            {item.comment_count} izoh
           </p>
-          
         </div>
       </div>
 
